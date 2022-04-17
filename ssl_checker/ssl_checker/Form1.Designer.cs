8c95c4edcba96a6f72ca6d3ec5028f9873c2d367
99Eyad99 <93145488+99Eyad99@users.noreply.github.com>
2022-04-17 17:29:11 +0300

    Add files via upload

---
+++ ssl_checker/ssl_checker/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ssl_checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.websites = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // websites
+            // 
+            this.websites.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.websites.Location = new System.Drawing.Point(12, 93);
+            this.websites.Multiline = true;
+            this.websites.Name = "websites";
+            this.websites.Size = new System.Drawing.Size(252, 245);
+            this.websites.TabIndex = 0;
+            this.websites.TextChanged += new System.EventHandler(this.websites_TextChanged);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(313, 93);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(457, 245);
+            this.result.TabIndex = 1;
+            this.result.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Add websites";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(309, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Result";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.websites);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox websites;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ssl_checker/ssl_checker/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ssl_checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string input;
+        string[] input_array;
+
+
+
+        static DateTime ReadExpirDate(string website)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(website);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Close();
+
+            X509Certificate cert = request.ServicePoint.Certificate;
+            X509Certificate2 cert2 = new System.Security.Cryptography.X509Certificates.X509Certificate2(cert);
+
+            string cedate = cert2.GetExpirationDateString();
+            return DateTime.Parse(cedate);
+        }
+
+        static int GetReminderDays(DateTime date)
+        {
+            DateTime thisDay = DateTime.Today;
+            TimeSpan difference = thisDay - date;
+            int days = (int)difference.TotalDays;
+
+            return days;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            input = websites.Text;
+            input_array = Regex.Split(input, "\r\n|\r|\n");
+            string output = "";
+
+         
+            foreach (string website in input_array)
+            {
+
+
+                try
+                {
+
+                    DateTime expireDate = ReadExpirDate(website);
+                    int days = GetReminderDays(expireDate);
+
+                    output += "Expire at: " + expireDate + " after " + Math.Abs(days) + " days \r\n ";
+
+                }catch(Exception ex)
+                {
+                    output += "Error message:" + ex.Message+ " \r\n";
+                }
+
+               
+
+
+            }
+
+            result.Text = output;
+
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void websites_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
